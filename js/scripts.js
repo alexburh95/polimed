f7cbbebe9eee41cdd3029c112047b1ce89673def
alexburh95 <alexburh@gmail.com>
2021-05-27 10:05:14 +0300

    прролбуем слайдеры

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,56 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+var swiper = new Swiper(".mySwipermob1", {
+
+  autoplay: {
+    delay: 3000,
+  },
+  slidesPerView: 2,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    }
+  }
+
+});
+var swiper = new Swiper(".mySwipermob2", {
+
+  autoplay: {
+    delay: 3000,
+  },
+  slidesPerView: 3,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+  
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    420: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    }
+  }
+
+});
