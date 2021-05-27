8d11641a967a08a665ba7652854531eb8611b7e1
alexburh95 <alexburh@gmail.com>
2021-05-27 16:45:42 +0300

    Загрузка моб векрсии

--- js/openPopup.js
+++ js/openPopup.js
@@ -1,29 +1,5 @@
-const openPopupMenu = (parrent) =>{
-
-
-    const container = parrent;
-    const button = container.querySelector('button');
-    const krest = container.querySelector('.krest');
-    const popup = container.querySelector('.os_popup')
-    const popupButton = popup.querySelector('button')
-  console.log(button)
-button.addEventListener('click',() =>{
-    popup.classList.remove('hidden')
-})
-krest.addEventListener('click',() =>{
-    popup.classList.add('hidden')
-})
-
-
-
-}
 const header = document.querySelector('header')
 const feedback = document.querySelector('.feedback')
-console.log(feedback)
-openPopupMenu(header)
-openPopupMenu(feedback)
-
-
 const mob_mebu_button = document.querySelector('.mob_header_btn')
 const mob_menu_popup = document.querySelector('.mob_menu')
 const close_menu_popup = document.querySelector('.back')
@@ -49,3 +25,31 @@ popup.classList.remove('hidden')
 krest.addEventListener('click',() =>{
 popup.classList.add('hidden')
 })
+
+
+const openPopupMenu = (parrent) =>{
+
+
+    const container = parrent;
+    const button = container.querySelector('button');
+    const krest = container.querySelector('.krest');
+    const popup = container.querySelector('.os_popup')
+    const popupButton = popup.querySelector('button')
+  console.log(button)
+button.addEventListener('click',() =>{
+    popup.classList.remove('hidden')
+})
+krest.addEventListener('click',() =>{
+    popup.classList.add('hidden')
+})
+
+
+
+}
+
+console.log(feedback)
+openPopupMenu(header)
+openPopupMenu(feedback)
+
+
+
